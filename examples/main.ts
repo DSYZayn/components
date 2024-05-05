e8275debfbe59a245101e42249ec828573bca61a
zayn <1546492103@qq.com>
2024-05-05 22:52:56 +0800

    test gpu.js

--- examples/main.ts
+++ examples/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import '@arcgis/core/assets/esri/themes/light/main.css'
 import { createPinia } from 'pinia'
+import { GPU } from 'gpu.js';
 import '@/assets/style.css';
 
 const app = createApp(App)
+const gpu = new GPU();
 app.use(createPinia())
+app.provide<GPU>('gpu', gpu);
 app.mount('#app')
---
+++ examples/utils/demo.ts
@@ -0,0 +1,7 @@
+
+
+// usage
+// const a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// const b = [[9, 8, 7], [6, 5, 4], [3, 2, 1]];
+// const c = multiplyMatrix(a, b) as number[][];
+// console.log(c);
